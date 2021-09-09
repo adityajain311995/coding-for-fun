76c936e0fc8a21449f193d0bd3bc5ac293e371dc
Aditya Jain <adityajain311995@gmail.com>
2021-09-09 13:56:08 +0530

    update

--- walmart/src/main/java/fun/coding/walmart/config/SwaggerConfig.java
+++ walmart/src/main/java/fun/coding/walmart/config/SwaggerConfig.java
@@ -1,12 +1,9 @@
 package fun.coding.walmart.config;
 
-import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
--- walmart/src/main/java/fun/coding/walmart/controller/HelloWorldController.java
+++ walmart/src/main/java/fun/coding/walmart/controller/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
     @GetMapping
     public String helloWorld() {
-        return "Hellow World :)";
+        return "Hello World :)";
     }
 
 }
